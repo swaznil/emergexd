c0f71f82a99ef40e719ff3aa479f556cf0aa2fbd
Swapnil <72032487+swaznil@users.noreply.github.com>
2026-06-22 23:42:20 +0545

    Added nav link and updated comments

--- main.js
+++ main.js
@@ -6,13 +6,22 @@ canvas.height = window.innerHeight;
 
 const particles = [];
 
+// Some obvious comments are kept for learning and understading
+
 for (let i = 0; i < 200; i++){
     particles.push({
         x: Math.random() * canvas.width,
         y: Math.random() * canvas.height,
 
-        vx: (Math.random() - 0.5) * 2,
-        vy: (Math.random() - 0.5) * 2,
+        vx: (Math.random() - 0.5) * 2, // range -1 to 1
+        vy: (Math.random() - 0.5) * 2, 
+        
+        /* 
+        vx < 0 = right, vx > 0 = left
+        vy < 0 = up, vy > 0 = down 
+        because screens are stored row-by-row 
+        from the top-left corner.
+        */
 
         color: "white"
     })
@@ -22,7 +31,7 @@ function update() {
     for (const p of particles){
         p.x += p.vx;
         p.y += p.vy;
-
+        // Bounce off of borders (It changes direction due to * -1)
         if (p.x < 0 || p.x > canvas.width) p.vx *= -1
         if (p.y < 0 || p.y > canvas.height) p.vy *= -1
     }
